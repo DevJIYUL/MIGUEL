3c7e990315c393901d3dc874e4dfcb9af8b01612
miguel <lnacles@naver.com>
2019-01-28 17:22:08 +0900

    [define method and basic things]

--- basic.py
+++ basic.py
@@ -1,1 +1,12 @@
-﻿
+﻿from PIL import Image
+from pytesseract import *
+
+def tesseract(imgfile, lang = 'kor'):
+    im = Image.open(imgfile)
+    text = image_to_string(im, lang=lang)
+
+    print('Letis go ocr result')
+    print(text)
+
+tesseract('ocr_test.jpg')
+#OCR('koream_ocr_test.png',lang='kor')
